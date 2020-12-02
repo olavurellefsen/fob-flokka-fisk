710ab2e26bfb16b93bee948b376dcf708d8be1e0
Kristmund Ryggstein <kristmunds@hotmail.com>
2020-12-02 14:32:56 +0000

    fixed typos

--- src/custom/data.js
+++ src/custom/data.js
@@ -1,52 +1,52 @@
 export const COMICS = {
-  DC: 'Botnsjóvarfiskur',
-  MARVEL: 'Uppsjóvarfiskur',
+  DC: 'Botnfiskur',
+  MARVEL: 'Uppisjóvarfiskur',
 };
 
 export const HEROES = [
   {
     name: 'Toskur',
-    comics: 'Botnsjóvarfiskur',
+    comics: 'Botnfiskur',
     rank: 0,
   },
   {
     name: 'Brosma',
-    comics: 'Botnsjóvarfiskur',
+    comics: 'Botnfiskur',
     rank: 0,
   },
   {
     name: 'Hýsa',
-    comics: 'Botnsjóvarfiskur',
+    comics: 'Botnfiskur',
     rank: 0,
   },
   {
     name: 'Longa',
-    comics: 'Botnsjóvarfiskur',
+    comics: 'Botnfiskur',
     rank: 0,
   },
   {
     name: 'Svartkalvi',
-    comics: 'Botnsjóvarfiskur',
+    comics: 'Botnfiskur',
     rank: 0,
   },
   {
     name: 'Makrelur',
-    comics: 'Uppsjóvarfiskur',
+    comics: 'Uppisjóvarfiskur',
     rank: 0,
   },
   {
     name: 'Sild',
-    comics: 'Uppsjóvarfiskur',
+    comics: 'Uppisjóvarfiskur',
     rank: 0,
   },
   {
     name: 'Svartkjaftur',
-    comics: 'Uppsjóvarfiskur' || "Botnsjóvarfiskur",
+    comics: 'Uppisjóvarfiskur',
     rank: 0,
   },
   {
     name: 'Upsi',
-    comics: 'Uppsjóvarfiskur' || "Botnsjóvarfiskur",
+    comics: "Botnfiskur",
     rank: 0,
   },
 ];
